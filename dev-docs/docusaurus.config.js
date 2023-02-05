c2d457b275f707d7f7db821de5736cb23a12cdf6
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-02-04 22:03:23 -0500

    Update docusaurus.config.js

--- dev-docs/docusaurus.config.js
+++ dev-docs/docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   onBrokenMarkdownLinks: "warn",
   favicon: "img/board.png",
   organizationName: "Iftiin-Board", // Usually your GitHub org/user name.
-  projectName: "iftiin-base", // Usually your repo name.
+  projectName: "iftiin-Board", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
